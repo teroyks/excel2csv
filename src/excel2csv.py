f312ca648147e654223a9eea4a6df6ee31bf7c72
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2019-03-19 15:10:20 +0200

    Add create unique names for sheets
    - take the xls(x) filename and directory as base
    
    - add sheetname (sanitized to alphanumeric characters) and .csv extentsion:

--- src/excel2csv.py
+++ src/excel2csv.py
@@ -1,7 +1,13 @@
 import argparse
 import pandas as pd
+import os
 
 from tqdm import tqdm
+
+
+def sanitize_filename(str):
+    return "".join(char for char in str if char.isalnum())
+
 
 parser = argparse.ArgumentParser(description='Excel to CSV converter')
 parser.add_argument('excel_file', type=argparse.FileType(
@@ -9,9 +15,14 @@ parser.add_argument('excel_file', type=argparse.FileType(
 
 args = parser.parse_args()
 filename = args.excel_file.name
+basename, _ = os.path.splitext(filename)
 
 file = pd.ExcelFile(filename)
 sheets = file.sheet_names
 
-for sheet in tqdm(sheets):
-    pass
+sheetfiles = []
+for sheet in tqdm(file.sheet_names):
+    sheet_filename = f'{basename}_{sanitize_filename(sheet)}.csv'
+    sheetfiles.append(sheet_filename)
+
+print(sheetfiles)
